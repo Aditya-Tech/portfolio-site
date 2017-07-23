a0c19243f7f18519b9547f1bc7522211cbf626f4
Aditya Paruchuri <adityaparuchuri@Adityas-MacBook-Pro.local>
2017-07-23 15:15:15 -0400

    added more personal info, side-panel, custom scrollbar

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var i = 1;
+var current = "#intro"
 const roles = ["\xa0developer\xa0\xa0", "\xa0designer\xa0\xa0\xa0", "entrepreneur"];
 
 $(document).ready(function() {
-
   setInterval(function() {
     $("#role").fadeOut(function() {
       $(this).text(roles[i]);
@@ -12,5 +12,34 @@ $(document).ready(function() {
       }
     }).fadeIn();
   }, 2000)
-
 });
+
+function hideCurrent(cb) {
+  $(current).fadeOut(cb);
+}
+
+function hideSidePanel() {
+  $("#side-panel").slideUp(function() {
+    $("#closed-side-panel").show();
+  });
+}
+
+function openSidePanel() {
+  $("#closed-side-panel").fadeOut(function() {
+    $("#side-panel").show();
+  });
+}
+
+function showCoursework() {
+  hideCurrent(function() {
+    current = "#coursework";
+    $(current).fadeIn();
+  })
+}
+
+function showAllscriptsInfo() {
+  hideCurrent(function() {
+    current = "#allscripts";
+    $(current).fadeIn();
+  })
+}
